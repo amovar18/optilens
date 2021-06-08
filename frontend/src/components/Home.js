b4550a41b3cfde4f4b7e2bb99b4c5afcd26a32a6
amovar18 <kgamoghavarsha@gmail.com>
2021-06-08 11:54:06 +0530

    changed static images folder and path

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,15 +7,15 @@ function Home(){
 		<div>
 			<div className="row justify-content-start">
 				<div className="col-sm">
-					<Link to="/product/contact_lens/1"><img src={"/contact-lenses.jpg"} alt='contact lens' className="gallery"/></Link>
+					<Link to="/product/contact_lens/1"><img src={"/images/contact-lenses.jpg"} alt='contact lens' className="gallery"/></Link>
 					<br/>
 				</div>
 				<div className="col-sm">
-					<Link to="/product/sunglasses/1"><img src={"/sunglasses.jpg"} alt='sunglasses' className="gallery"/></Link>
+					<Link to="/product/sunglasses/1"><img src={"/images/sunglasses.jpg"} alt='sunglasses' className="gallery"/></Link>
 					<br/>
 				</div>
 				<div className="col-sm">
-					<Link to="/product/spectacles/1"><img src={"/spectacles.jpg"} alt='spectacles' className="gallery"/></Link>
+					<Link to="/product/spectacles/1"><img src={"/images/spectacles.jpg"} alt='spectacles' className="gallery"/></Link>
 					<br/>
 				</div>
 			</div>
