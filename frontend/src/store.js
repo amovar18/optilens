6dc0da6f656eb1174dcff221a2fc97ce3e28c597
amovar18 <kgamoghavarsha@gmail.com>
2021-06-08 10:31:51 +0530

    fixing header issue

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,12 @@
-import {configureStore, combineReducers} from "@reduxjs/toolkit";
+import {configureStore, combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
 import cartReducer from './slices/cart/cartSlice';
 import authenticationReducer from './slices/authentication/authenticationSlice';
 import productReducer from './slices/product/productSlice';
 import transactionReducer from './slices/transaction/transactionSlice';
 import sellerReducer from './slices/seller/sellerSlice';
 import orderReducer from './slices/order/orderSlice';
-import {persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-
+import {persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER} from 'redux-persist'
 const rootReducer = combineReducers({
     cart:cartReducer,
     authentication:authenticationReducer,
@@ -23,5 +22,10 @@ const persistConfig = {
 const persistedReducer = persistReducer({persistConfig,rootReducer});
 const store = configureStore({
     reducer:persistedReducer,
+    middleware: getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
 })
 export default store;
