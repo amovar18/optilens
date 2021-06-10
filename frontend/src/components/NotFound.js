967a2d36e7aa05cbbd11885603fa76abc01649b4
amovar18 <kgamoghavarsha@gmail.com>
2021-06-10 13:32:39 +0530

    added timed redirection

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import history from '../history';
 export default function NotFound(){
+    setTimeout(() => {
+        history.push('/');
+    }, 3000);
     return(
         <div className='container-fluid' style={{'backgroundColor':'#D3D3D3','height':'100vh'}}>
             <div className='row' style={{'height':'100vh'}}>
                 <div className='col' />
                 <div className='col align-self-center'>
                     <h1>404: Not Found Redirecting you to home.</h1>
-                    {setTimeout(() => {
-                        history.push('/');
-                    }, 3000)}
+                    
                 </div>
                 <div className='col'/>
             </div>
